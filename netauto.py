be51d8c5cdea560ad0e1fb9134e100dedc0e6ae4
Evan Goulden <evan.j.goulden@gmail.com>
2025-03-17 10:28:22 +0000

    Updated configuration to update ConnectionOptions to allow secret being added to the inventory as a data element

--- netauto.py
+++ netauto.py
@@ -1,6 +1,6 @@
-from nornir import InitNornir
-from nornir.core.task import Task, Result, MultiResult
 from pprint import pprint as pp
+from nornir import InitNornir
+from nornir.core.inventory import ConnectionOptions
 from nornir_napalm.plugins.tasks import napalm_get
 
 
@@ -11,11 +11,27 @@ def netauto():
     pp(f'''sbx Devices: {nr.filter(env="sbx").inventory.hosts}''')
     pp(f'''IOS Devices: {nr.filter(net_os="ios").inventory.hosts}''')
     sbx_devices = nr.filter(env="sbx")
+    print(type(sbx_devices))
     sbx_devices.run(task=backup_config)
 
 
-
 def backup_config(task):
+    secret = task.host.get("secret")
+    task.host.connection_options["napalm"] = ConnectionOptions(
+        extras={
+            "optional_args": {```
+                "secret": secret,
+            }
+        },
+        hostname=task.host.hostname,
+        username=task.host.username,
+        password=task.host.password,
+        platform=task.host.platform,
+        port=task.host.port,
+    )
+
+    if not secret:
+        raise ValueError("No secret set for device")
     result = task.run(task=napalm_get, getters=["config"])
     running_config = result.result.get("config", {}).get("running", "")
 
